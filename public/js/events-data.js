c76a7723cfce0ba037dff444fbd8c3ea4e255640
panforrest <guoqianp@gmail.com>
2017-12-23 12:34:10 -0500

    minor change a couple of dates

--- public/js/events-data.js
+++ public/js/events-data.js
@@ -1,6 +1,6 @@
 var canvasEvents = {
 	'12-24-2017' : '<a href="https://www.eventbrite.com/e/original-e-village-food-culture-tourtm-52-tickets-37899477357?lang=en-us&locale=en_US&view=domo" target=_blank>Original E. Village Food & Culture Tour™ $52</a>',
 	'12-28-2017' : '<a href="https://www.eventbrite.com/e/sale-tastes-of-chinatown-tour-5799-5999-guided-by-local-chinese-tickets-14815366159" target=_blank>TASTES OF CHINATOWN - GUIDED ONLY BY LOCAL CHINESE</a>',
-	'01-15-2018' : '<a href="https://www.eventbrite.com/e/sale-ultimate-new-york-food-culture-tourtm-4699-52-in-the-village-tickets-37899543555?lang=en-us&locale=en_US&view=domo" target=_blank>Ultimate New York Food & Culture Tour™ </a>',
-	'01-31-2018' : '<a href="https://www.eventbrite.com/d/ny--brooklyn/sale%21-gourmet-and-fusion-dessert-tour%E2%84%A2-%2459.99/?q=+Sale%21+Gourmet+%26+Fusion+Dessert+Tour%E2%84%A2+%2459.99&mode=search" target=_blank>Gourmet & Fusion Dessert Tour</a>',
+	'01-03-2018' : '<a href="https://www.eventbrite.com/e/sale-ultimate-new-york-food-culture-tourtm-4699-52-in-the-village-tickets-37899538540?lang=en-us&locale=en_US&view=domo" target=_blank>Ultimate New York Food & Culture Tour™ </a>',
+	'01-07-2018' : '<a href="https://www.eventbrite.com/e/sale-gourmet-fusion-dessert-tourtm-5999-tickets-34449971793?aff=es2" target=_blank>Gourmet & Fusion Dessert Tour</a>',
 };
